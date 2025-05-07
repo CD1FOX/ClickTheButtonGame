2fe7ffe5c13dc869950b81f78215ecacefe60af5
Francis Leo <drypasta34@gmail.com>
2025-05-07 12:39:32 +0800

    Update
    Added:
    - Difficulty Scaling

--- main.gd
+++ main.gd
@@ -3,6 +3,16 @@ extends Node2D
 #add a variable to count the clicks
 var clickCount = 0
 
+var shrinkAlready = false
+
+func ShrinkSize():
+	if not shrinkAlready:
+		$Button.size.x -= 20
+		$Button.size.y -= 20
+	
+	shrinkAlready = true;
+
+
 func RandomLocations():
 	var randomNumberX = randi_range(21, 928)
 	var randomNumberY = randi_range(59, 555)
@@ -20,7 +30,12 @@ func _process(_delta: float) -> void:
 	
 	#TimerLeft
 	$TimeLeftLabel.text = "TIME: " + str(int($TimeLeftTimer.time_left))
-
+	
+	if clickCount % 10 == 0 and clickCount != 0:
+		ShrinkSize()
+	else:
+		shrinkAlready = false
+		
 func _on_button_pressed() -> void:
 	clickCount += 1
 
